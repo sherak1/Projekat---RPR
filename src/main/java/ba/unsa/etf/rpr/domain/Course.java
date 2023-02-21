c9280fd2da3bbda0b6c1a10e392ed2800d2c1763
Sanina <sherak1@etf.unsa.ba>
2023-02-21 20:24:03 +0100

    dodana klasa Course i njeni atributi

--- src/main/java/ba/unsa/etf/rpr/domain/Course.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Course.java
@@ -1,4 +1,18 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.time.LocalDate;
+
 public class Course implements Idable {
+    /**
+     * Attributes for class Course
+     */
+    private int id;
+    private String course_name;
+    private String lecture_number;
+    private LocalDate lecture_date;
+    private String exercise_number;
+    private LocalDate exercise_date;
+    private Student student; //key
+
+
 }
