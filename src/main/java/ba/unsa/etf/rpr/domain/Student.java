7492b7b0e3fdc307ab114fd4f7160b65e36291d9
Sanina <sherak1@etf.unsa.ba>
2023-02-21 18:06:41 +0100

    dodani getteri i setteri u klasi Student

--- src/main/java/ba/unsa/etf/rpr/domain/Student.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Student.java
@@ -31,6 +31,44 @@ public class Student implements Idable {
      */
     public Student() {
     }
+    /**
+     * Getters for attributes
+     * @return
+     */
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getIndex_number() {
+        return index_number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    /**
+     * Setters for attributes
+     * @return
+     */
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setIndex_number(String index_number) {
+        this.index_number = index_number;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     @Override
     public void setId(int id) {
