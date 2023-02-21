d19b8cc780eeb859b2fc32e4070a2f4fccc5d343
Sanina <sherak1@etf.unsa.ba>
2023-02-21 20:32:45 +0100

    dodani setteri i getteri klase Course

--- src/main/java/ba/unsa/etf/rpr/domain/Course.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Course.java
@@ -36,6 +36,54 @@ public class Course implements Idable {
     public Course() {
     }
 
+    public String getCourse_name() {
+        return course_name;
+    }
+
+    public void setCourse_name(String course_name) {
+        this.course_name = course_name;
+    }
+
+    public String getLecture_number() {
+        return lecture_number;
+    }
+
+    public void setLecture_number(String lecture_number) {
+        this.lecture_number = lecture_number;
+    }
+
+    public LocalDate getLecture_date() {
+        return lecture_date;
+    }
+
+    public void setLecture_date(LocalDate lecture_date) {
+        this.lecture_date = lecture_date;
+    }
+
+    public String getExercise_number() {
+        return exercise_number;
+    }
+
+    public void setExercise_number(String exercise_number) {
+        this.exercise_number = exercise_number;
+    }
+
+    public LocalDate getExercise_date() {
+        return exercise_date;
+    }
+
+    public void setExercise_date(LocalDate exercise_date) {
+        this.exercise_date = exercise_date;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
     @Override
     public void setId(int id) {
 
