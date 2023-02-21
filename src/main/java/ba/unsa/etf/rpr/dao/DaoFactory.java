277f216cf43b18685f548a946391da8df3abeeda
Sanina <sherak1@etf.unsa.ba>
2023-02-22 00:13:04 +0100

    dodane dodatne klase u dao folderu i metode

---
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -0,0 +1,41 @@
+package ba.unsa.etf.rpr.dao;
+
+
+
+/**
+ * DaoFactory class that make instances of dao classes
+ */
+public class DaoFactory {
+
+    /**
+     * Static methods for DaoFactory
+     */
+    private static final TeacherDao teacherDao = new TeacherDaoSQLImpl();
+    private static  final StudentDao studentDao = new StudentDaoSQLImpl();
+    private static final CourseDao courseDao = new CourseSQLImpl();
+
+    /**
+     * Constructor without parameters
+     */
+    private DaoFactory() {}
+
+    /**
+     * Method for access TeacherDao instance
+     * @return
+     */
+    public static TeacherDao teacherDao() {
+        return  teacherDao;
+    }
+
+    /**
+     * Method for access StudentDao instance
+     * @return
+     */
+    public static StudentDao studentDao() { return studentDao; }
+
+    /**
+     * Method for access CoruseDao instance
+     * @return
+     */
+    public static CourseDao courseDao() {return courseDao; }
+}
