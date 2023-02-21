dca68760ff90324f120c5df419bd194cde0dcbbd
Sanina <sherak1@etf.unsa.ba>
2023-02-22 00:56:13 +0100

    edit StudentDao i StudentDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/StudentDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/StudentDao.java
@@ -1,4 +1,28 @@
 package ba.unsa.etf.rpr.dao;
 
-public interface StudentDao {
+import ba.unsa.etf.rpr.domain.Student;
+import ba.unsa.etf.rpr.domain.Course;
+import ba.unsa.etf.rpr.exceptions.HappyCourseException;
+
+import java.util.List;
+
+/**
+ * Interface that extends Dao<T> and also has methods for StudentDao
+ */
+public interface StudentDao extends Dao<Student> {
+
+    /**
+     * Method that find all students
+     * @return list of all students
+     * @throws HappyCourseException
+     */
+    public List<Student> allStudents() throws HappyCourseException;
+
+    /**
+     * Method that find all students based on index_number
+     * @param index_number
+     * @return
+     * @throws HappyCourseException
+     */
+    public Student getByIndex_number(String index_number) throws HappyCourseException;
 }
--- src/main/java/ba/unsa/etf/rpr/dao/StudentDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/StudentDaoSQLImpl.java
@@ -1,4 +1,38 @@
 package ba.unsa.etf.rpr.dao;
 
-public class StudentDaoSQLImpl implements StudentDao {
+import ba.unsa.etf.rpr.domain.Student;
+import ba.unsa.etf.rpr.exceptions.HappyCourseException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+public class StudentDaoSQLImpl extends AbstractDao<Student> implements StudentDao {
+
+
+    @Override
+    public Student row2object(ResultSet rs) throws HappyCourseException, SQLException {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> object2row(Student Object) {
+        return null;
+    }
+
+    @Override
+    public Student getById(int id) throws HappyCourseException {
+        return null;
+    }
+
+    @Override
+    public List<Student> allStudents() throws HappyCourseException {
+        return null;
+    }
+
+    @Override
+    public Student getByIndex_number(String index_number) throws HappyCourseException {
+        return null;
+    }
 }
