3a0e7be2945ece18c347f95b3a31471fd6ef2ddc
Sanina <sherak1@etf.unsa.ba>
2023-02-21 20:30:09 +0100

    dodana konstruktor sa svim parametrima klase Course

--- src/main/java/ba/unsa/etf/rpr/domain/Course.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Course.java
@@ -13,6 +13,26 @@ public class Course implements Idable {
     private String exercise_number;
     private LocalDate exercise_date;
     private Student student; //key
+    /**
+     * Constructor with all parameters
+     * @param id
+     * @param course_name
+     * @param lecture_number
+     * @param lecture_date
+     * @param exercise_number
+     * @param exercise_date
+     * @param student
+     */
+    public Course(int id, String course_name, String lecture_number, LocalDate lecture_date, String exercise_number, LocalDate exercise_date, Student student) {
+        this.id = id;
+        this.course_name = course_name;
+        this.lecture_number = lecture_number;
+        this.lecture_date = lecture_date;
+        this.exercise_number = exercise_number;
+        this.exercise_date = exercise_date;
+        this.student = student;
+    }
+
 
 
     @Override
