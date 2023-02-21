44809c25508bff2ec184097e3939c94256952d66
Sanina <sherak1@etf.unsa.ba>
2023-02-21 17:55:59 +0100

    dodan konstruktor sa svim parametrima u klasi Student

--- src/main/java/ba/unsa/etf/rpr/domain/Student.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Student.java
@@ -5,10 +5,26 @@ public class Student implements Idable {
      * Attributes for class Student
      */
     private int id;
-    private String firstName;
-    private String lastName;
+    private String first_name;
+    private String last_name;
     private String index_number;
     private String address;
+
+    /**
+     * Constructor with all parameters
+     * @param id
+     * @param first_name
+     * @param last_name
+     * @param index_number
+     * @param address
+     */
+    public Student(int id, String first_name, String last_name, String index_number, String address) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.index_number = index_number;
+        this.address = address;
+    }
 
     @Override
     public void setId(int id) {
