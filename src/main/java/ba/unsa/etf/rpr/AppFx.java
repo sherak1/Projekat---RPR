09f480aa003ea79a2a487a3521b224955f9e7e55
Sanina <sherak1@etf.unsa.ba>
2023-02-21 17:34:18 +0100

    dodane klase domain

---
+++ src/main/java/ba/unsa/etf/rpr/AppFx.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppFX extends Application {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/home.fxml"));
+        primaryStage.setTitle("Happy course");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
