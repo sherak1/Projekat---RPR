922aa4f6ae8f994b2bca3d811c18ff286e40c517
Sanina <sherak1@etf.unsa.ba>
2023-02-22 23:29:13 +0100

    addStudent fxml added

--- src/main/java/ba/unsa/etf/rpr/business/StudentManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/StudentManager.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Student;
+import ba.unsa.etf.rpr.exceptions.HappyCourseException;
+
 public class StudentManager {
+
+
+    public static Student add(Student student) throws HappyCourseException {
+
+
+        try {
+            return DaoFactory.studentDao().add(student);
+        } catch (HappyCourseException e) {
+            throw e;
+        }
+    }
 }
