23ade10a02d4e98afd8da5b2da24483702c2436b
Sanina <sherak1@etf.unsa.ba>
2023-02-23 00:19:27 +0100

    add Course fxml

--- src/main/java/ba/unsa/etf/rpr/controllers/TeacherController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/TeacherController.java
@@ -1,17 +1,24 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Student;
 import ba.unsa.etf.rpr.domain.Teacher;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javafx.scene.control.ListView;
 import java.io.IOException;
+import java.util.List;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class TeacherController {
+    public ListView listView;
 
     private Teacher teacher=new Teacher();
     public TeacherController() {
@@ -20,6 +27,22 @@ public class TeacherController {
 
     public TeacherController(Teacher teacher) {
         this.teacher = teacher; }
+
+    public void initialize() {
+        try {
+            ObservableList studentItems = FXCollections.observableArrayList();
+            List<Student> studentsList = DaoFactory.studentDao().allStudents();
+
+            for (int i = 0; i < studentsList.size(); i++) {
+                studentItems.add(studentsList.get(i).getFirst_name()+" "+ studentsList.get(i).getLast_name());
+            }
+            listView.setItems(studentItems);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
         public void showProfile(ActionEvent event) {
             try {
@@ -46,7 +69,7 @@ public class TeacherController {
             stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
             stage.setResizable(false);
             stage.show();
-            StudentController studentController = loader.getController();
+            //StudentController studentController = loader.getController();
            // listView.refresh();
            // criminalController.setList(listView);
         } catch (IOException e) {
