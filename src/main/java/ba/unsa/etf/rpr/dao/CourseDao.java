2225a1518946b26ed5c1c7f1660807ab1e3d91da
Sanina <sherak1@etf.unsa.ba>
2023-02-22 01:34:41 +0100

    CourseDao interface

--- src/main/java/ba/unsa/etf/rpr/dao/CourseDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CourseDao.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr.dao;
 
-public interface CourseDao {
+import ba.unsa.etf.rpr.domain.Course;
+import ba.unsa.etf.rpr.exceptions.HappyCourseException;
+
+import java.util.List;
+
+/**
+ * Interface that extends Dao<T> and also has methods only for Course Dao
+ */
+public interface CourseDao extends Dao<Course> {
+
+    /**
+     * Method for getting all courses based on student id
+     * @param id
+     * @return list of all courses records with specified id
+     * @throws HappyCourseException
+     */
+    public List<Course> getByIdNew(int id) throws HappyCourseException;
+
+    public Course getByExercise_number(String exercise_number) throws HappyCourseException;
 }
