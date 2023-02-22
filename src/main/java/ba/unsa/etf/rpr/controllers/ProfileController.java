7549734355f708f632ef0edf126bf68112a424a7
Sanina <sherak1@etf.unsa.ba>
2023-02-22 23:26:09 +0100

    Student manager added

--- src/main/java/ba/unsa/etf/rpr/controllers/ProfileController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ProfileController.java
@@ -1,4 +1,40 @@
 package ba.unsa.etf.rpr.controllers;
 
+
+import ba.unsa.etf.rpr.domain.Teacher;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+
 public class ProfileController {
+
+ Teacher teacher = new Teacher();
+    public Label welcomeLabel;
+    public TextField firstNameFld;
+    public TextField lastNameFld;
+    public TextField emailFld;
+    public TextField usernameFld;
+    public PasswordField passwordFld;
+
+    public Button updateBtn;
+    public ProfileController() {
+        teacher = new Teacher ();
+    }
+    public ProfileController(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+
+    public void initialize() {
+        if (teacher!= null) {
+            welcomeLabel.setText("Welcome, " + teacher.getFirst_name());
+            firstNameFld.setText(teacher.getFirst_name());
+            lastNameFld.setText(teacher.getLast_name());
+            emailFld.setText(teacher.getEmail());
+            usernameFld.setText(teacher.getUsername());
+            passwordFld.setText(teacher.getPassword());
+        }
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/controllers/TeacherController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/TeacherController.java
@@ -1,14 +1,39 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.domain.Teacher;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class TeacherController {
+
     private Teacher teacher=new Teacher();
     public TeacherController() {
         teacher = new Teacher();
     }
 
     public TeacherController(Teacher teacher) {
-        this.teacher = teacher;
-    }
+        this.teacher = teacher; }
+
+        public void showProfile(ActionEvent event) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/profile.fxml"));
+                loader.setController(new ProfileController(teacher));
+                Parent root = loader.load();
+                Stage stage = new Stage();
+                stage.setTitle("Students");
+                stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+                stage.setResizable(false);
+                stage.show();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
 }
