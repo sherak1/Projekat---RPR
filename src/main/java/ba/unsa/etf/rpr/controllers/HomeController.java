e24cd8deaf2126b74b922f3d182d143336b2c62f
Sanina <sherak1@etf.unsa.ba>
2023-02-22 19:11:39 +0100

    about added and edit

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -7,13 +7,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class HomeController {
     public Button regButton;
+    public Button loginButton;
 
+    public Button aboutButton;
 
     public void showRegistration(ActionEvent event) {
         try {
@@ -43,6 +46,23 @@ public class HomeController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+        public void showAbout(ActionEvent event) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/about.fxml"));
+                loader.setController(new HomeController());
+                Parent root = loader.load();
+                Stage stage = new Stage();
+                stage.setTitle("About");
+                stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+                stage.setResizable(false);
+                stage.show();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
+
     }
 
 
