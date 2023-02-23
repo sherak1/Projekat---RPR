7f3c3af27e5efb8bbcadad6d7d687c77f795fc1c
Sanina <sherak1@etf.unsa.ba>
2023-02-23 03:57:19 +0100

    studentTest added for getter and setters and toString

--- src/test/java/ba/unsa/etf/rpr/domain/StudentTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/StudentTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class CriminalTest {
+public class StudentTest {
 
     private Student student = new Student();
 
@@ -17,7 +17,7 @@ public class CriminalTest {
      */
     @Test
     void getFirstName() {
-        assertEquals("Student", student.getFirst_name()));
+        assertEquals("Student", student.getFirst_name());
     }
 
     /**
@@ -27,6 +27,25 @@ public class CriminalTest {
     void getLastName() {
         assertEquals("Student", student.getLast_name());
     }
+    /**
+     * Test which tests Student class getter for attribute index_number
+     */
+    @Test
+    void getIndex_number() {
+        assertEquals("18957", student.getIndex_number());
+    }
 
+    /**
+     * Test which tests Student class method toString
+     */
+    @Test
+    void toStringTest() {
+        String s = "First name: " + student.getFirst_name() + "\nLast name: " + student.getLast_name() + "\nIndex: " + student.getIndex_number() + "\nAddress: " + student.getAddress();
+        assertEquals(s, student.toString());
+    }
 }
+
+
+
+
 
