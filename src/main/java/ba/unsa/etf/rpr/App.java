61c3c6687aa073f8473c0ed6542cc7be730127e4
Sanina <sherak1@etf.unsa.ba>
2023-02-23 11:04:28 +0100

    edit App

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -15,7 +15,7 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class App  {
 
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Happy Courses Application!" );
 
     }
 
