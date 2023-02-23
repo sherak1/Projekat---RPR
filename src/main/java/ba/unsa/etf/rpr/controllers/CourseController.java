95e2ed166ed1dd4e0540bfec2a7f2af75a134d98
Sanina <sherak1@etf.unsa.ba>
2023-02-23 02:48:17 +0100

    CourseController addCourse added

--- src/main/java/ba/unsa/etf/rpr/controllers/CourseController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/CourseController.java
@@ -5,14 +5,19 @@ import ba.unsa.etf.rpr.business.StudentManager;
 import ba.unsa.etf.rpr.domain.Course;
 import ba.unsa.etf.rpr.exceptions.HappyCourseException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class CourseController {
-    private Course course;
+
     private CourseManager courseMan = new CourseManager();
 
     public TextField courseNameFld;
@@ -21,28 +26,29 @@ public class CourseController {
     public TextField exerciseNumberFld;
     public DatePicker date2Fld;
     public Button addCourseBtn;
-    public  CourseController(Course course) {
-        this.course = course;
-    }
 
-
-    public CourseController() {
-        course = new Course();
-    }
-    public void addNewCourse(ActionEvent event) {
+    public void addCourse(ActionEvent event) {
         try {
             Course course = new Course();
             course.setId(1);
-            course.setCourse(course);
+          //  course.setCourse(coursee);
             course.setCourse_name(courseNameFld.getText());
             course.setLecture_number(lectureNumberFld.getText());
             course.setLecture_date(date1Fld.getValue());
             course.setExercise_number(exerciseNumberFld.getText());
             course.setExercise_date(date2Fld.getValue());
+           // courseMan.add(course);
+            Stage stage1 = (Stage) addCourseBtn.getScene().getWindow();
+            stage1.close();
             courseMan.add(course);
-            Stage stage = (Stage) addCourseBtn.getScene().getWindow();
-            stage.close();
-        } catch (HappyCourseException e) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/addCourse.fxml"));
+            loader.setController(new StudentController());
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+        } catch (Exception e) {
             new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
         }
     }
