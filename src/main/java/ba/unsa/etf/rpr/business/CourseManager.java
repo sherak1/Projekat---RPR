3a3ff4ba1c348d0652ad76c54b2a315fc771bfae
Sanina <sherak1@etf.unsa.ba>
2023-02-23 02:28:13 +0100

    CourseController method addNewCourse added

--- src/main/java/ba/unsa/etf/rpr/business/CourseManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/CourseManager.java
@@ -1,4 +1,8 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.domain.Course;
+
 public class CourseManager {
+    public void add(Course course) {
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/controllers/CourseController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/CourseController.java
@@ -1,24 +1,52 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.CourseManager;
+import ba.unsa.etf.rpr.business.StudentManager;
 import ba.unsa.etf.rpr.domain.Course;
+import ba.unsa.etf.rpr.exceptions.HappyCourseException;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class CourseController {
-    public void addNewRecord(ActionEvent event) {
+    private Course course;
+    private CourseManager courseMan = new CourseManager();
+
+    public TextField courseNameFld;
+    public TextField lectureNumberFld;
+    public DatePicker date1Fld;
+    public TextField exerciseNumberFld;
+    public DatePicker date2Fld;
+    public Button addCourseBtn;
+    public  CourseController(Course course) {
+        this.course = course;
+    }
+
+
+    public CourseController() {
+        course = new Course();
+    }
+    public void addNewCourse(ActionEvent event) {
         try {
             Course course = new Course();
             course.setId(1);
             course.setCourse(course);
-            course.setPlace(placeFld.getText());
-            course.setDate(dateFld.getValue());
-            course.setCode(codeFld.getText());
-            course.setDescription(descriptionFld.getText());
-            criminalRecordMan.add(course);
-            Stage stage = (Stage) addRecordBtn.getScene().getWindow();
+            course.setCourse_name(courseNameFld.getText());
+            course.setLecture_number(lectureNumberFld.getText());
+            course.setLecture_date(date1Fld.getValue());
+            course.setExercise_number(exerciseNumberFld.getText());
+            course.setExercise_date(date2Fld.getValue());
+            courseMan.add(course);
+            Stage stage = (Stage) addCourseBtn.getScene().getWindow();
             stage.close();
-        } catch (CriminalRecordsException e) {
+        } catch (HappyCourseException e) {
             new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
         }
     }
 
 }
+
+
